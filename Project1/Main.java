b8b0058269dcc3ceac9d5be418da765cc0c2804d
[Hunterstokes] <hunterstokes@me.com>
2023-10-25 22:54:06 -0700

    project 11

---
+++ Project1/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+  static public void main(String[] args) {
+    
+
+    //prompt user for input
+    System.out.println("Enter a number.");
+    System.out.print("Number must be 0, 1 or 2: ");
+
+
+    Scanner scnr = new Scanner(System.in);
+    int usersNumber = scnr.nextInt();
+
+    // initialize the program number
+    int programsGuess;
+    Random rand = new Random();
+    programsGuess = rand.nextInt(0, 3);
+    
+    // check the numbers
+    if (usersNumber == programsGuess) {
+      System.out.println("It was " + usersNumber + "!?");
+      System.out.print("I was right!");
+    } else {
+      System.out.print("Argh! I was wrong! I guessed " + programsGuess);
+    }
+
+
+  }
+}
---
+++ zylabProblems/LAB2.24.java
@@ -0,0 +1,21 @@
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner scnr = new Scanner(System.in);
+//         int ageYears;
+//         int weightPounds;
+//         int heartRate;
+//         int timeMinutes;
+//         double caloriesBurned;
+
+//         ageYears = scnr.nextInt();
+//         weightPounds = scnr.nextInt();
+//         heartRate = scnr.nextInt();
+//         timeMinutes = scnr.nextInt();
+
+//         caloriesBurned = ((ageYears * 0.2757) + (weightPounds * 0.03295) + (heartRate * 1.0781) - 75.4991) * timeMinutes / 8.368;
+
+//         System.out.printf("%.2f", caloriesBurned);
+//     }
+// }
