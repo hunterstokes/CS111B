bf50bbc7415c98b97f09be243361d72e7db31511
Hunter Brown <hunterstokes@me.com>
2023-12-04 11:56:01 -0800

    _

---
+++ Project11/GridWriterExpanded/Square.java
@@ -0,0 +1,20 @@
+package Project11.GridWriterExpanded;
+
+public class Square extends GridItem {
+    
+    private int side;
+
+    public Square(int xValue, int yValue, int s) {
+        x = xValue;
+        y = yValue;
+        side = s;
+    }
+
+    public double getArea() {
+        return Math.pow(side, 2);
+    }
+
+    public boolean containsPoint(int xValue, int yValue) {
+        return xValue >= x && xValue <= x + side && yValue >= y && yValue <= y + side;
+    }
+}
